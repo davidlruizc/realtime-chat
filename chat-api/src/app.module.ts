9bbcfe95869f2cc0d9370186856c2ade79718ff8
David Ruiz <davidl-ruizc@outlook.com>
2020-11-17 17:55:28 -0500

    connection db

--- chat-api/src/app.module.ts
+++ chat-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  imports: [],
+  imports: [
+    TypegooseModule.forRoot('mongodb://root:root@localhost/realtime-chat', {}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
